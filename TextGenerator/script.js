a780c1ef449788bf45f9fa89b9553ced59fbfcbd
Saarim Shahid <shamy103@mymail.unisa.edu.au>
2023-06-19 17:35:55 +1000

    back button added

--- TextGenerator/script.js
+++ TextGenerator/script.js
@@ -5,7 +5,10 @@ function forward() {
    text = prompt("Text to repeat: ");
    repeat = prompt("How many times you want to repeat the text");
    repeat = parseInt(repeat);
+   writeBackButton(); // Call the function to write the back button
+   writeBootstrapStylesheet(); // Call the function to write the Bootstrap stylesheet
    document.write('<style>body { background-color: #15202b; }</style>'); // Set background color
+
    for (i = 1; i <= repeat; i++) {
       document.write('<p style="margin:0;color:aqua;">' + i + ". " + text + "</p>");
    }
@@ -18,6 +21,8 @@ function backwards() {
    text = prompt("Text to repeat: ");
    repeat = prompt("How many times you want to repeat the text");
    repeat = parseInt(repeat);
+   writeBackButton(); // Call the function to write the back button
+   writeBootstrapStylesheet(); // Call the function to write the Bootstrap stylesheet
    document.write('<style>body { background-color: #15202b; }</style>'); // Set background color
    for (i = repeat; i >= 0; i--) {
       document.write('<p style="margin:0;color:aqua;">' + i + ". " + text + "</p>");
@@ -35,7 +40,8 @@ function downhill() {
    newText = text.charAt(currentChar);
    repeat = prompt("How many times you want to repeat the text");
    repeat = parseInt(repeat);
-
+   writeBackButton(); // Call the function to write the back button
+   writeBootstrapStylesheet(); // Call the function to write the Bootstrap stylesheet
    document.write('<style>body { background-color: #15202b; }</style>'); // Set background color
    document.write('<p style="margin:0;color:aqua;">'); // Set paragraph style
 
@@ -62,7 +68,8 @@ function vertical() {
    newText = text.charAt(currentChar);
    repeat = prompt("How many times you want to repeat the text");
    repeat = parseInt(repeat);
-
+   writeBackButton(); // Call the function to write the back button
+   writeBootstrapStylesheet(); // Call the function to write the Bootstrap stylesheet
    document.write('<style>body { background-color: #15202b; }</style>'); // Set background color
    document.write('<p style="margin:0;color:aqua;">'); // Set paragraph style
 
@@ -76,4 +83,17 @@ function vertical() {
    }
 
    document.write('</p>'); // Close the paragraph
+}
+
+
+function writeBackButton() {
+   document.write('<div style="margin-top: 100px; margin-bottom: 10px;"><button class="btn btn-light" onclick="goBack()">Go Back</button></div>');
+}
+
+function writeBootstrapStylesheet() {
+   document.write('<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous" />');
+}
+
+function goBack() {
+   location.reload(); // Reload the current page
 }
